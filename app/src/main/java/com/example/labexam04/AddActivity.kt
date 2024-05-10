4df99f935d7cab97afb9371e396fb35f55152ba6
Dhanuka Chamalitha <dhanukawickramasinghe1024@gmail.com>
2024-05-10 11:26:58 +0530

    Create insert data to the db

--- app/src/main/java/com/example/labexam04/AddActivity.kt
+++ app/src/main/java/com/example/labexam04/AddActivity.kt
@@ -1,17 +1,21 @@
 package com.example.labexam04
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.labexam04.databinding.ActivityAddBinding
 import com.example.labexam04.databinding.ActivityMainBinding
 
 class AddActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAddBinding
     private lateinit var db: TaskDBHelper
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add)
@@ -21,14 +25,21 @@ class AddActivity : AppCompatActivity() {
             insets
         }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = TaskDBHelper(this)
 
-        binding.addBtn.setOnClickListener{
-            val title = binding.titleEditText.text.toS
+        binding.saveBtn.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val description = binding.descriptionEditText.text.toString()
+            val task = Task(0, title, description)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
         }
+
+
 
     }
 }
